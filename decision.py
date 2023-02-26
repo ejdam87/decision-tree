6f1424168405cb9b345f8b9257b8e4589c42e1a5
ejdam87 <adam.dzadon@gmail.com>
2023-02-26 15:16:17 +0100

    Finish decision tree

--- decision.py
+++ decision.py
@@ -1,46 +1,94 @@
 import gain
 import pandas as pd
-from typing import List
 
 GOAL = "Tenis"
 CHART = pd.read_excel( "tenis.xlsx" )
 
+Attribute = str
+
 
 class Node:
-    def __init__( self, is_leaf: bool=False, val: str="" ) -> None:
+    def __init__( self, is_leaf: bool, attr: Attribute ) -> None:
         self.is_leaf = is_leaf
-        self.val = val
-        self.children: List[ "Node" ] = []
+        self.attr = attr  # if is_leaf, this represent result class
+        self.parent: "Node" | None = None
+        self.children: dict[ str, "Node" ] = {} # { atribute value -> child node }
 
-    def draw( self, indent: int=0 ) -> str:
+    def draw( self, indent: int=0, attr_val: str="" ) -> None:
+
         print( indent * " ", end="" )
-        print( self.val )
-        for child in self.children:
-            child.draw( indent + 2 )
+
+        attr = self.parent.attr if self.parent is not None else ""
+        attr_draw = f"( {attr}={attr_val} )" if attr != "" else ""
+        val = self.attr + "?" if not self.is_leaf else self.attr
+        print( val, attr_draw )
+
+        for attr_val, child in self.children.items():
+            child.draw( indent + 4, attr_val )
+
+class DecisionTree:
+    def __init__( self, root: Node ) -> None:
+        self.root = root
+
+    def get_root( self ) -> Node:
+        return self.root
+
+    def draw( self ) -> None:
+        self.root.draw()
 
 
-def best_attr( data: pd.DataFrame ) -> str:
+def best_attr( data: pd.DataFrame ) -> Attribute:
+    """
+    Function to find attribute with the highest information gain from data
+    """
     gains = [ ( gain.gain( data, attr, GOAL ), attr ) for attr in data if attr != GOAL ]
     return max( gains )[ 1 ]
 
 
-def split_by( data: pd.DataFrame, attr: str ) -> List[ pd.DataFrame ]:
-    
+def split_by( data: pd.DataFrame, attr: Attribute ) -> list[ tuple[ str, pd.DataFrame ] ]:
+    """
+    Function to split data on given attr ( i.e. creates new dataframes for every value of given attr )
+    """
     vals = gain.get_distr( data, attr )
-    return [ data.loc[ data[ attr ] == val ] for val in vals ]
+    return [ ( val, data.loc[ data[ attr ] == val ] ) for val in vals ]
 
 
-def decision_tree( data: pd.DataFrame, attr_val: str="" ) -> Node:
+def decision_tree( data: pd.DataFrame ) -> DecisionTree:
+    """
+    Returns decision tree for given data
+    """
+    root = decision_tree_rec( data, "", set() )
+    return DecisionTree( root )
 
+
+def decision_tree_rec( data: pd.DataFrame, attr_val: str, seen_attrs: set[ str ] ) -> Node:
+    """
+    Function which returns a partial decision tree node for given data
+    """
     classes = data[ GOAL ].unique()
+
+    ## If all examples are from single class
     if len( classes ) == 1:
-        return Node( is_leaf=True, val=f"{classes[ 0 ]} ({attr_val})" )
+        return Node( is_leaf=True, attr=classes[ 0 ] )
 
     attr = best_attr( data )
-    new = Node( val=f"{attr}? ({attr_val}) ")
-    for partition in split_by( data, attr ):
-        new.children.append( decision_tree( partition ) )
+
+    ## If we already splitted on this attr -> there will never be exactly one class
+    ## so we create leaf node with most common value from examples
+    if attr in seen_attrs:
+        most_common = data[ GOAL ].mode()   ## Can be more values
+        return Node( is_leaf=True, attr=most_common[0] )
+
+    seen_attrs.add( attr )
+
+    new = Node( is_leaf=False, attr=attr )
+
+    for attr_val, partition in split_by( data, attr ):
+        child = decision_tree_rec( partition, attr_val, seen_attrs )
+        child.parent = new
+        new.children[ attr_val ] = child
 
     return new
 
-decision_tree( CHART ).draw()
+tree = decision_tree( CHART )
+tree.draw()
